77d3c0e6e1bcc8e77c09d5942dba49789c38a5c0
Amit Yadav <87850377+Amit-Yadav21@users.noreply.github.com>
2021-09-09 19:59:25 +0530

    Add files via upload

---
+++ imp.py
@@ -0,0 +1,28 @@
+# counter = 1
+# while counter < 100:
+#     if counter % 2 == 0:
+#         print (counter)
+#     counter = counter + 1
+
+
+# iterable=[1,2]
+# for word in iterable:
+#     print(' ',word)
+#     iterable.append(word)
+
+
+# i=1
+# while i<=20:
+#     print(i)
+#     i=i+1
+
+# i=1
+# while i<=20:
+#     print(-i)
+#     i+=1
+
+
+i=0
+while i>=(-20):
+    print(i)
+    i=i-1
